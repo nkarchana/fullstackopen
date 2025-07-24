31e9ff30b0dab5ca39d8a7a8b2fabec6932b1506
Archana <naik.archana93@gmail.com>
2025-07-24 16:23:42 +0200

    adding phonebook backend

---
+++ part3/phonebook backend/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+const logData = (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    JSON.stringify(req.body),
+  ].join(" ");
+};
+
+app.use(morgan(logData));
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const response = `<div>Phonebook has info for ${
+    persons.length
+  } people</div><p>${new Date()}</p>`;
+  res.send(response);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name or number missing" });
+  }
+  if (persons.some((p) => p.name === body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+  const newPerson = {
+    id: (Math.floor(Math.random() * 999999) + 1).toString(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
